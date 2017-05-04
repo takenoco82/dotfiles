9bf4f8f0701463590f4496d86341b44d44798c42
Daisuke TAKEUCHI <takenoco82@gmail.com>
2017-05-04 19:27:37 +0900

    Zsh の環境を新規構築した。

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -sf ~/git/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
