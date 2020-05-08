1c2d9f4e20932998594dba34e51b4c9148920f7f
Daisuke TAKEUCHI <takenoco82@gmail.com>
2020-05-08 23:06:46 +0900

    [fix] インストール用スクリプトを修正

--- install.sh
+++ install.sh
@@ -1,30 +1,21 @@
-#!/bin/sh
-ln -sf ~/git/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/git/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/git/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/git/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/git/dotfiles/.myclirc ~/.myclirc
+#!/bin/bash
 
-zsh_comp_dir=~/.zsh/completions
-zsh_completion=/Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion
-docker_compose_zsh_completion=/Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion
-mkdir -p $zsh_comp_dir
-if [ -e $zsh_completion ]; then
-    ln -sf $zsh_completion $zsh_comp_dir/_docker
-fi
-if [ -e $docker_compose_zsh_completion ]; then
-    ln -sf $docker_compose_zsh_completion $zsh_comp_dir/_docker-compose
-fi
+#
+# dotfiles に対し、ホームディレクトリにシンボリックリンクを貼る
+#
 
-# install starship
-starship --version > /dev/null
-if [[ $? -ne 0 ]]; then
-  brew install starship
-fi
+set -eu
 
-config_dir=~/.config
-mkdir -p $config_dir
-ln -sf ~/git/dotfiles/.config/starship.toml ~/.config/starship.toml
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOTFILES="${SCRIPT_DIR}/.??*"
+
+for dotfile in $DOTFILES
+do
+    local file_name=$(basename "$dotfile")
+    [[ $file_name == ".DS_Store" ]] && continue
+    [[ $file_name == ".git" ]] && continue
+    [[ $file_name == ".gitignore" ]] && continue
+
+    # echo "$dotfile"
+    ln -snfv "$dotfile" "${HOME}"/"${file_name}"
+done
