8a9af6f1e551159f29621a985ddfc4fa1d3f0bc0
Daisuke TAKEUCHI <takenoco82@gmail.com>
2020-05-09 12:18:15 +0900

    [fix] 配置場所を変更

--- setup/macos.sh
+++ setup/macos.sh
@@ -68,7 +68,35 @@ function setup_trackpad() {
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
 }
 
+function setup_system_preferences() {
+  echo "System Preferences"
+
+  setup_dock
+  setup_finder
+  setup_battery
+  setup_datetime
+  setup_security_privacy
+  setup_keyboard
+  setup_trackpad
+
+  killall Dock
+  killall Finder
+  killall SystemUIServer
+
+  echo "System Preferences completed successfully"
+}
+
+function install_xcode_command_line_tools() {
+  echo "Instaling Xcode Command Line Tools"
+
+  xcode-select --install
+
+  echo "Instaling Xcode Command Line Tools completed successfully"
+}
+
 function install_package_manager() {
+  echo "Instaling Package Manager"
+
   # # Homebrewでインストールする際のパーミンションエラー対策
   # sudo chown -R $(whoami):admin /usr/local
   # sudo chmod -R g+w /usr/local
@@ -79,80 +107,79 @@ function install_package_manager() {
 
   brew install cask
   brew install mas
+
+  echo "Instaling Package Manager completed successfully"
+}
+
+function install_packages() {
+  echo "Instaling Packages"
+
+  brew install awscli
+  brew install git
+  brew install mycli
+  brew install poetry
+  brew install pyenv
+  brew install starship
+  brew install vim
+
+  brew cask install alfred
+  # brew cask install amazon-workspaces
+  brew cask install appcleaner
+  brew cask install dash
+  brew cask install docker
+  brew cask install google-backup-and-sync
+  brew cask install google-chrome
+  brew cask install google-japanese-ime
+  brew cask install hyperswitch
+  brew cask install iterm2
+  brew cask install microsoft-teams
+  brew cask install mysqlworkbench
+  brew cask install onedrive
+  brew cask install spectacle
+  brew cask install tableplus
+  brew cask install visual-studio-code
+  brew cask install vlc
+  brew cask install webex-meetings
+
+  mas instal 568494494  # Pocket
+  mas instal 803453959  # Slack
+  mas instal 1278508951  # Trello
+
+  echo "Instaling Packages completed successfully"
+}
+
+function install_fonts() {
+  echo "Instaling Fonts"
+
+  brew tap homebrew/cask-fonts
+  brew cask install font-source-han-code-jp
+
+  echo "Instaling Fonts completed successfully"
 }
 
 # https://hnakamur.github.io/blog/2015/04/06/install-apps-without-homebrew-cask/
 function install_keyhac() {
+  echo "Instaling Keyhac"
+
   local download_url=http://crftwr.github.io/keyhac/mac/download/Keyhac-1.10.dmg
   local dmg_file=${download_url##*/}
 
   curl -LO $download_url
-  mount_dir=`hdiutil attach $dmg_file | awk -F '\t' 'END{print $NF}'`
+  local mount_dir=`hdiutil attach $dmg_file | awk -F '\t' 'END{print $NF}'`
   sudo /usr/bin/ditto "$mount_dir/Keyhac.app" "/Applications/Keyhac.app"
   hdiutil detach "$mount_dir"
   rm $dmg_file
+
+  echo "Instaling Keyhac completed successfully"
 }
 
+function main() {
+  setup_system_preferences
+  install_xcode_command_line_tools
+  install_package_manager
+  install_packages
+  install_fonts
+  install_keyhac
+}
 
-# System Preferences
-setup_dock
-setup_finder
-setup_battery
-setup_datetime
-setup_security_privacy
-setup_keyboard
-setup_trackpad
-
-killall Dock
-killall Finder
-killall SystemUIServer
-
-
-# Install Xcode Command Line Tools
-xcode-select --install
-
-
-# Install Package Manager (Homebrew, Homebrew-Cask, mas-cli)
-install_package_manager
-
-
-# Install applications
-brew install awscli
-brew install git
-brew install mycli
-brew install poetry
-brew install pyenv
-brew install starship
-brew install vim
-
-brew cask install alfred
-# brew cask install amazon-workspaces
-brew cask install appcleaner
-brew cask install dash
-brew cask install docker
-brew cask install google-backup-and-sync
-brew cask install google-chrome
-brew cask install google-japanese-ime
-brew cask install hyperswitch
-brew cask install iterm2
-brew cask install microsoft-teams
-brew cask install mysqlworkbench
-brew cask install onedrive
-brew cask install spectacle
-brew cask install tableplus
-brew cask install visual-studio-code
-brew cask install vlc
-brew cask install webex-meetings
-
-mas instal 568494494  # Pocket
-mas instal 803453959  # Slack
-mas instal 1278508951  # Trello
-
-
-# Insstall Fonts
-brew tap homebrew/cask-fonts
-brew cask install font-source-han-code-jp
-
-
-# Install dmg package
-install_keyhac
+main
